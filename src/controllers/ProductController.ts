7628d911388ce30eb888ad70102da1b505e37655
Gustavo <gustavo.prog40@gmail.com>
2021-06-19 11:49:06 -0300

    changes-git-repo

---
+++ src/controllers/ProductController.ts
@@ -0,0 +1,73 @@
+import { Request, Response } from 'express'
+import productModel from '../models/productModel'
+const { promisify } = require('util')
+import fs from 'fs'
+const unlink = promisify(fs.unlink)
+
+async function getAll(req: Request, res: Response) {
+  try {
+    const data = await productModel.find()
+    return res.status(200).send(data)
+  } catch (error) {
+    return res.status(400).send({ message: 'DEU ERRO!' })
+  }
+}
+
+async function ProductRegister(req: Request, res: Response) {
+  try {
+    const { filename: image } = req.file
+
+    const [name] = image.split('.')
+    const filename = `${name}.jpg`
+
+    await productModel.create({
+      title: req.body.title,
+      price: req.body.price,
+      image: filename,
+    })
+
+    return res.status(201).send({ message: 'Product Created with success!' })
+  } catch (error) {
+    return res.status(400).send({ error })
+  }
+}
+
+async function Update(req: Request, res: Response) {
+  try {
+    await productModel.findByIdAndUpdate(req.params.id, {
+      $set: {
+        title: req.body.title,
+        price: req.body.price,
+        image: req.body.image,
+      },
+    })
+
+    return res.status(201).send({ message: 'Product Editado com success!' })
+  } catch (error) {
+    return res.status(400).send({ error })
+  }
+}
+
+async function deleteOne(req: Request, res: Response) {
+  try {
+    const { id } = req.params
+
+    const imagem = await productModel.findById(id)
+
+    console.log(imagem.image)
+
+    console.log('entrou')
+    await productModel.findByIdAndDelete(id)
+
+    fs.unlink(`uploads/${imagem.image}`, (err) => {
+      if (err) throw err
+      console.log('uploads/file.txt was deleted')
+    })
+
+    return res.status(201).send({ message: 'Product Deleted with success!' })
+  } catch (error) {
+    return res.status(400).send({ Mensagem: 'All cagado!!', error })
+  }
+}
+
+export default { getAll, ProductRegister, deleteOne, Update }
